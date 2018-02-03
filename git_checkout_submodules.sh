cf6352c0a567451a8a06bb87eabe76ab87ae9108
dorgon.chang <dorgonman@hotmail.com>
2018-02-03 11:38:45 +0800

    up

--- git_checkout_submodules.sh
+++ git_checkout_submodules.sh
@@ -1,6 +1,6 @@
+GIT_BRANCH=$(git symbolic-ref --short HEAD)
 
-
-git submodule foreach --recursive "git checkout -f feature/core || :"
+git submodule foreach --recursive "git checkout -f ${GIT_BRANCH} || :"
 git submodule foreach --recursive "git pull"
 
 
