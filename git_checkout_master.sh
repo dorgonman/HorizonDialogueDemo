73642b441bf3e025c15b0edd3e1f028063395bf3
dorgon.chang <dorgonman@hotmail.com>
2018-09-27 00:11:53 +0800

    up

--- git_checkout_master.sh
+++ git_checkout_master.sh
@@ -4,3 +4,8 @@ git checkout -f master
 git pull
 git submodule foreach --recursive "git checkout -f master"
 git submodule foreach --recursive "git pull"
+
+pushd ci_scripts
+
+./install_nuget_package_local.sh
+popd 
