8119e6b095055e6623dcc3551243eddcd1f5fc19
dorgon chang <dorgonman@hotmail.com>
2018-11-03 20:54:44 +0800

    refactor DataTable folder to DataTableFromExcel

--- DesignerRawData/export_excel.sh
+++ DesignerRawData/export_excel.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
-BASE_PATH=$(pwd)
-OUTPUT_DIR="${BASE_PATH}/../Content/HorizonDialogueDemo/DataTable/"
-mkdir -p ${OUTPUT_DIR}
-OUTPUT_DIR=$(cd "${OUTPUT_DIR}"; pwd)
-#rm -rf ${OUTPUT_DIR}/*
+# BASE_PATH=$(pwd)
+# OUTPUT_DIR="${BASE_PATH}/../Content/HorizonDialogueDemo/DataTableFromExcel/"
+# mkdir -p ${OUTPUT_DIR}
+# OUTPUT_DIR=$(cd "${OUTPUT_DIR}"; pwd)
+# #rm -rf ${OUTPUT_DIR}/*
 
 ./export_excel_step_0.sh
 
